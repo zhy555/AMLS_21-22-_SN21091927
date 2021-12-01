5ee6376e45490a0c918f7bbcd674215647c08d8b
zhy555 <zhanghaoyang_apply@163.com>
2021-12-01 12:58:03 +0000

    linear_regression

---
+++ AMLS/lab_1_linear_regression_part_SOLUTIONS.ipynb/linear_regression.py
@@ -0,0 +1,8 @@
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn.metrics import  mean_squared_error, r2_score
+
