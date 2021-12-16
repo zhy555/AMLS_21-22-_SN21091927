d91ea4949e19c55e1ec54f5a3d2d1134531394b6
zhy555 <zhanghaoyang_apply@163.com>
2021-12-16 09:18:15 +0000

    linear_regression

--- AMLS/Final/task1/task1_main.py
+++ AMLS/Final/task1/task1_main.py
@@ -62,7 +62,7 @@ cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', inp
 # Step 2 - Pooling
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 
-# Adding a second convolutional layer
+# Adding a second convolutional layershui
 cnn.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'))
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 
